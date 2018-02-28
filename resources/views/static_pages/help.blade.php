638f6d18f42148ba15013994e625978cfc550df1
Lyn <wenzhan.main@gmail.com>
2018-02-28 03:31:54 -0800

    help page

--- resources/views/static_pages/help.blade.php
+++ resources/views/static_pages/help.blade.php
@@ -17,18 +17,27 @@
             <div class="panel-body">
                <div class="container-fluid">
                   <div class="row">
-                     <div class="col-xs-10 col-xs-offset-1">
-                        <h5>当前数据</h5>
+                     <div class="col-xs-10 col-xs-offset-1 h6">
+                       <?php $helps = $data['webinfo_help'];  ?>
+                       @foreach ($helps as $i=>$help)
+                        <div class="row">
+                          <a data-toggle="collapse" data-target="#part{{$i}}" class="h4">{{$help[0]}}</a>
+                          <div id="part{{$i}}" class="collapse h6 col-xs-offset-1">
+                            {!! Markdown::convertToHtml($help[1]) !!}
+                          </div>
+                        </div>
+                       @endforeach
                         <br>
-                        <p>信息每页显示：{{ $data['items_per_page'] }}个</p>
-                        <p>信息每分区显示：{{ $data['items_per_part'] }}个</p>
-                        <p>目录每页显示：{{ $data['index_per_page'] }}个</p>
-                        <p>目录每分区显示：{{ $data['index_per_part'] }}个</p>
-                        <p>长评标准：{{ $data['longcomment_lenth'] }}字</p>
-                        <p>新章节的更新字数必须达到{{ $data['update_min'] }}字，才能计入本书“最后更新”的排名数据（顶帖）</p>
-                        <br>
-                        {!! Markdown::convertToHtml($data['webinfo_help']) !!}
-                        <p></p>
+                       <a data-toggle="collapse" data-target="#settings" class="h4">当前数据</a>
+                       <div id="settings" class="collapse h6 col-xs-offset-1">
+                         <p>信息每页显示：{{ $data['items_per_page'] }}个</p>
+                         <p>信息每分区显示：{{ $data['items_per_part'] }}个</p>
+                         <p>目录每页显示：{{ $data['index_per_page'] }}个</p>
+                         <p>目录每分区显示：{{ $data['index_per_part'] }}个</p>
+                         <p>长评标准：{{ $data['longcomment_lenth'] }}字</p>
+                         <p>新章节的更新字数必须达到{{ $data['update_min'] }}字，才能计入本书“最后更新”的排名数据（顶帖）</p>
+                       </div>
+
                      </div>
                   </div>
                </div>
