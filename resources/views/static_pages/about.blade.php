e2e67bbddf32cc85f41f50d10120c0bf5679bdee
Lyn <wenzhan.main@gmail.com>
2018-03-15 15:58:36 -0700

    trial

--- resources/views/static_pages/about.blade.php
+++ resources/views/static_pages/about.blade.php
@@ -10,6 +10,7 @@
                   <div class="row">
                      <div class="col-xs-10 col-xs-offset-1">
                         <h1>关于</h1>
+                        test
                      </div>
                   </div>
                </div>
