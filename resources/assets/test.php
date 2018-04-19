8da146665ca57fac747155bca7176b0ff4d8c37a
Lyn <wenzhan.main@gmail.com>
2018-04-19 16:16:05 -0700

    cron

--- resources/assets/test.php
+++ resources/assets/test.php
@@ -1,2 +1,5 @@
 <?php
 DB::table('users')->where('deleted_at','=',null)->update(['users.experience_points' => DB::raw("jifen")]);
+
+cron command
+* * * * * /opt/php71/bin/php /home4/sosad/sketch/artisan schedule:run >> /dev/null 2>&1
