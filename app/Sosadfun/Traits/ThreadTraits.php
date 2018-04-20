46e623cfb84b66858760d21fc09953acae56d304
Lyn <wenzhan.main@gmail.com>
2018-04-19 19:45:11 -0700

    thread_id

--- app/Sosadfun/Traits/ThreadTraits.php
+++ app/Sosadfun/Traits/ThreadTraits.php
@@ -19,7 +19,7 @@ trait ThreadTraits{
     }
 
     public function return_thread_fields($query){
-        $query->select('threads.*', 'books.*', 'users.name', 'labels.labelname', 'channels.channelname', 'posts.body as last_post_body', 'tongrens.tongren_yuanzhu',  'tongrens.tongren_cp', 'tongrens.tongren_yuanzhu_tag_id', 'tongrens.tongren_cp_tag_id', 'chapters.title as last_chapter_title', 'chapters.responded as last_chapter_responded', 'chapters.post_id as last_chapter_post_id', 'tongren_yuanzhu_tags.tagname as tongren_yuanzhu_tagname', 'tongren_cp_tags.tagname as tongren_cp_tagname');
+        $query->select('books.*','threads.*', 'users.name', 'labels.labelname', 'channels.channelname', 'posts.body as last_post_body', 'tongrens.tongren_yuanzhu',  'tongrens.tongren_cp', 'tongrens.tongren_yuanzhu_tag_id', 'tongrens.tongren_cp_tag_id', 'chapters.title as last_chapter_title', 'chapters.responded as last_chapter_responded', 'chapters.post_id as last_chapter_post_id', 'tongren_yuanzhu_tags.tagname as tongren_yuanzhu_tagname', 'tongren_cp_tags.tagname as tongren_cp_tagname');
         return $query;
     }
 }
