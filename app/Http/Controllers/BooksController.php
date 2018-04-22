08023510900ee8371a406f6b558438a2a9c35b11
Lyn <wenzhan.main@gmail.com>
2018-04-21 22:31:28 -0700

    update with tags

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -31,39 +31,6 @@ class BooksController extends Controller
         $this->middleware('auth')->only('create', 'store', 'update','edit');
     }
 
-    public function all_book_tags(){
-        $tags=[];
-        $tags['labels_yuanchuang']= Cache::remember('-labels_yuanchuang-', 10, function () {
-            $labels_yuanchuang = Label::where('channel_id',1)->get();
-            return $labels_yuanchuang;
-        });
-        $tags['labels_tongren']=Cache::remember('-labels_tongren-', 10, function () {
-            $labels_tongren = Label::where('channel_id',2)->get();
-            return $labels_tongren;
-        });
-        $tags['tags_feibianyuan']=Cache::remember('-tags-feibianyuan-', 10, function () {
-            $tags_feibianyuan = Tag::where('tag_group',0)->get();
-            return $tags_feibianyuan;
-        });
-        $tags['tags_bianyuan']=Cache::remember('-tags-bianyuan-', 10, function () {
-            $tags_bianyuan = Tag::where('tag_group',5)->get();
-            return $tags_bianyuan;
-        });
-        $tags['tags_tongren']=Cache::remember('-tags-tongren-', 10, function () {
-            $tags_tongren = Tag::where('tag_group',25)->get();
-            return $tags_tongren;
-        });
-        $tags['tags_tongren_yuanzhu']=Cache::remember('-tags-tongren-yuanzhu-', 10, function () {
-            $tags_tongren_yuanzhu = Tag::where('tag_group',10)->get();
-            return $tags_tongren_yuanzhu;
-        });
-        $tags['tags_tongren_cp']=Cache::remember('-tags-tongren-cp-', 10, function () {
-            $tags_tongren_cp = Tag::where('tag_group',20)->get();
-            return $tags_tongren_cp;
-        });
-        return $tags;
-    }
-
     public function create()
     {
         $all_book_tags = $this->all_book_tags();
@@ -78,12 +45,13 @@ class BooksController extends Controller
     }
     public function edit(Book $book)
     {
+        $all_book_tags = $this->all_book_tags();
         if ((Auth::id() == $book->thread->user_id)&&(!$book->thread->locked)){
             $thread = $book->thread->load('mainpost');
             $book->load('tongren');
             $tags = $thread->tags->pluck('id')->toArray();
             $all_book_tags = $this->all_book_tags();
-            return view('books.edit',compact('book', 'thread','tags', $all_book_tags));
+            return view('books.edit',compact('book', 'thread','tags', 'all_book_tags'));
         }else{
             return redirect()->route('error', ['error_code' => '405']);
         }
--- app/Sosadfun/Traits/BookTraits.php
+++ app/Sosadfun/Traits/BookTraits.php
@@ -2,6 +2,7 @@
 namespace App\Sosadfun\Traits;
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
 
 trait BookTraits{
     public function join_book_tables(){
@@ -19,5 +20,38 @@ trait BookTraits{
     public function return_book_fields($query){
         $query->select('books.*', 'threads.*', 'tongrens.tongren_yuanzhu','tongrens.tongren_cp','tongrens.tongren_yuanzhu_tag_id','tongrens.tongren_cp_tag_id', 'users.name','labels.labelname', 'chapters.title as last_chapter_title', 'chapters.responded as last_chapter_responded', 'chapters.post_id as last_chapter_post_id','tongren_yuanzhu_tags.tagname as tongren_yuanzhu_tagname','tongren_cp_tags.tagname as tongren_cp_tagname');
         return $query;
+    }
+
+    public function all_book_tags(){
+        $tags=[];
+        $tags['labels_yuanchuang']= Cache::remember('-labels_yuanchuang-', 10, function () {
+            $labels_yuanchuang = Label::where('channel_id',1)->get();
+            return $labels_yuanchuang;
+        });
+        $tags['labels_tongren']=Cache::remember('-labels_tongren-', 10, function () {
+            $labels_tongren = Label::where('channel_id',2)->get();
+            return $labels_tongren;
+        });
+        $tags['tags_feibianyuan']=Cache::remember('-tags-feibianyuan-', 10, function () {
+            $tags_feibianyuan = Tag::where('tag_group',0)->get();
+            return $tags_feibianyuan;
+        });
+        $tags['tags_bianyuan']=Cache::remember('-tags-bianyuan-', 10, function () {
+            $tags_bianyuan = Tag::where('tag_group',5)->get();
+            return $tags_bianyuan;
+        });
+        $tags['tags_tongren']=Cache::remember('-tags-tongren-', 10, function () {
+            $tags_tongren = Tag::where('tag_group',25)->get();
+            return $tags_tongren;
+        });
+        $tags['tags_tongren_yuanzhu']=Cache::remember('-tags-tongren-yuanzhu-', 10, function () {
+            $tags_tongren_yuanzhu = Tag::where('tag_group',10)->get();
+            return $tags_tongren_yuanzhu;
+        });
+        $tags['tags_tongren_cp']=Cache::remember('-tags-tongren-cp-', 10, function () {
+            $tags_tongren_cp = Tag::where('tag_group',20)->get();
+            return $tags_tongren_cp;
+        });
+        return $tags;
     }
 }
