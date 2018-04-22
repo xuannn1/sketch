ec9aed31ca2ed33b9bf3fa1fe4d462c7f9e07e30
Lyn <wenzhan.main@gmail.com>
2018-04-22 13:52:07 -0700

    channel->sexual_orientation

--- app/Http/Controllers/ChannelsController.php
+++ app/Http/Controllers/ChannelsController.php
@@ -24,10 +24,12 @@ class ChannelsController extends Controller
         .'-'.($request->sexual_orientation? 'so'.$request->sexual_orientation:'no_so')
         .'-'.($request->page? 'page'.$request->page:'page1');
         $threads = Cache::remember($threadqueryid, 2, function () use($request, $channel) {
-            if($channel->id!=2){
-                $query = $this->join_no_book_thread_tables();
-            }else{
+            if($channel->id==1){
+                $query = $this->join_no_tongren_thread_tables();
+            }elseif($channel->id==2){
                 $query = $this->join_thread_tables();
+            }else{
+                $query = $this->join_no_book_thread_tables();
             }
             $query->where([['threads.channel_id','=',$channel->id],['threads.deleted_at', '=', null],['threads.public','=',1]]);
             if($request->label){$query = $query->where('threads.label_id','=',$request->label);}
@@ -36,10 +38,12 @@ class ChannelsController extends Controller
                 if($request->sexual_orientation){$query = $query->where('books.sexual_orientation','=',$request->sexual_orientation);}
             }
             if(!Auth::check()){$query = $query->where('threads.bianyuan','=',0);}
-            if($channel->id!=2){
-                $query = $this->return_no_book_thread_fields($query);
-            }else{
+            if($channel->id==1){
+                $query = $this->return_no_tongren_thread_fields($query);
+            }elseif($channel->id==2){
                 $query = $this->return_thread_fields($query);
+            }else{
+                $query = $this->return_no_book_thread_fields($query);
             }
             $threads = $query->orderby('threads.lastresponded_at', 'desc')
             ->paginate(config('constants.index_per_page'));
