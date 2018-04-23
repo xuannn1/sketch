57a94169da2491625dd135dedfc56da7a85df6ce
Lyn <wenzhan.main@gmail.com>
2018-04-23 01:05:25 -0700

    re-arrange pages

--- resources/views/long_comments/index.blade.php
+++ resources/views/long_comments/index.blade.php
@@ -1,8 +1,7 @@
 @extends('layouts.default')
 @section('title', '论坛')
-
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
         <div class="panel panel-default">
             <div class="panel-heading">
