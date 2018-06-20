525b428c2e3930c6f9c35adbd6bb4476a6f1f918
Lyn <wenzhan.main@gmail.com>
2018-06-20 12:10:01 -0700

    question_answer

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -15,63 +15,44 @@
             </div>
             <div class="panel-body">
                 @foreach ($questions as $question)
-                @if($question->answer_id>0)
-                <div class="row grayout">
+                <div class="row {{ $question->answer_id>0? 'grayout':'' }} main-text indentation">
                     <div class="col-xs-2">
                         {{ Carbon\Carbon::parse($question->created_at)->diffForHumans() }}提问
                     </div>
                     <div class="col-xs-10">
                         {!! Helper::wrapParagraphs($question->question_body) !!}
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-xs-2">
-                        {{ Carbon\Carbon::parse($question->answer->created_at)->diffForHumans() }}回答
-                        @if($question->answer->updated_at > $question->answer->created_at)
-                        {{ Carbon\Carbon::parse($question->answer->updated_at)->diffForHumans() }}修改
+                        @if((Auth::check())&&(Auth::id()==$user->id)&&($question->answer_id==0))
+                            <button onclick="document.getElementById('answeringquestion{{$question->id}}').style.display = 'block'" class="btn btn-primary btn-sm sosad-button pull-right">回答</button>
                         @endif
                     </div>
-                    <div class="col-xs-10">
-                        {!! Helper::wrapParagraphs($question->answer->answer_body) !!}
-                        @if((Auth::check())&&(Auth::id()==$user->id))
-                        <button onclick="document.getElementById('answeringquestion{{$question->id}}').style.display = 'block'" class="btn btn-primary btn-sm sosad-button pull-right">修改回答</button>
-                        @endif
-                    </div>
-                    <div id="answeringquestion{{$question->id}}" style="display:none">
-                        <form method="POST" action="{{ route('questions.answer', ['user'=>$user,'question'=>$question]) }}" name="create_answer_to_question">
-                            {{ csrf_field() }}
-                            <div class="form-group">
-                                <textarea id="answerofquestion{{$question->id}}" name="body" rows="10" class="form-control" data-provide="markdown" placeholder="问题正文">{{ $question->answer->answer_body }}</textarea>
-                            </div>
-                            <button type="submit" class="btn btn-primary sosad-button">发布回答</button>
-                            <br>
-                        </form>
-                    </div>
                 </div>
-                <hr>
-                @else
-                    @if((Auth::check())&&(Auth::id()==$user->id))
-                    <div class="row">
+                @if($question->answer_id>0)
+                    <div class="row main-text indentation">
                         <div class="col-xs-2">
-                            {{ Carbon\Carbon::parse($question->created_at)->diffForHumans() }}提问
+                            {{ Carbon\Carbon::parse($question->answer->created_at)->diffForHumans() }}回答
+                            @if($question->answer->updated_at > $question->answer->created_at)
+                            {{ Carbon\Carbon::parse($question->answer->updated_at)->diffForHumans() }}修改
+                            @endif
                         </div>
                         <div class="col-xs-10">
-                            {!! Helper::wrapParagraphs($question->question_body) !!}
-                            <button onclick="document.getElementById('answeringquestion{{$question->id}}').style.display = 'block'" class="btn btn-primary btn-sm sosad-button pull-right">回答</button>
-                        </div>
-                        <div id="answeringquestion{{$question->id}}" style="display:none">
-                            <form method="POST" action="{{ route('questions.answer', ['user'=>$user,'question'=>$question]) }}" name="create_answer_to_question">
-                                {{ csrf_field() }}
-                                <div class="form-group">
-                                    <textarea id="answerofquestion{{$question->id}}" name="body" rows="10" class="form-control" data-provide="markdown" placeholder="问题正文">{{ old('body') }}</textarea>
-                                </div>
-                                <button type="submit" class="btn btn-primary sosad-button">发布回答</button>
-                                <br>
-                            </form>
+                            {!! Helper::wrapParagraphs($question->answer->answer_body) !!}
+                            @if((Auth::check())&&(Auth::id()==$user->id))
+                            <button onclick="document.getElementById('answeringquestion{{$question->id}}').style.display = 'block'" class="btn btn-primary btn-sm sosad-button pull-right">修改回答</button>
+                            @endif
                         </div>
                     </div>
-                    @endif
                 @endif
+                <div id="answeringquestion{{$question->id}}" style="display:none">
+                    <form method="POST" action="{{ route('questions.answer', ['user'=>$user,'question'=>$question]) }}" name="create_answer_to_question">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <textarea id="answerofquestion{{$question->id}}" name="body" rows="10" class="form-control" data-provide="markdown" placeholder="问题正文">{{ $question->answer? $question->answer->answer_body : old('body') }}</textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary sosad-button">发布回答</button>
+                        <br>
+                    </form>
+                </div>
+                <hr>
                 @endforeach
                 {{ $questions->links() }}
             </div>
