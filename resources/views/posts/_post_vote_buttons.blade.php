5be514626635c5ccc2348ebf8b87f55e2af02a75
Lyn <wenzhan.main@gmail.com>
2018-06-28 11:23:58 -0700

    format for vote

--- resources/views/posts/_post_vote_buttons.blade.php
+++ resources/views/posts/_post_vote_buttons.blade.php
@@ -1,4 +1,4 @@
-<button class="btn btn-xs btn-info sosad-button" data-id="{{$post->id}}"  id = "{{$post->id.'upvote'}}" onclick="vote_post({{$post->id}},'upvote')" ><span class="glyphicon glyphicon-heart">{{ $post->up_voted }}</button>
+<button class="btn btn-default btn-xs" data-id="{{$post->id}}"  id = "{{$post->id.'upvote'}}" onclick="vote_post({{$post->id}},'upvote')" ><span class="glyphicon glyphicon-heart">{{ $post->up_voted }}</button>
 <!-- <button class="glyphicon glyphicon-thumbs-down vote" data-id="{{$post->id}}"  id = "{{$post->id.'downvote'}}" onclick="vote_post({{$post->id}},'downvote')">{{ $post->down_voted }}</button>
 <button class="glyphicon funny_post vote vote-text" data-id="{{$post->id}}"  id = "{{$post->id.'funny'}}" onclick="vote_post({{$post->id}},'funny')">搞笑{{ $post->funny }}</button>
 <button class="glyphicon fold_post vote vote-text" data-id="{{$post->id}}"  id = "{{$post->id.'fold'}}" onclick="vote_post({{$post->id}},'fold')">折叠{{ $post->fold }}</button> -->
