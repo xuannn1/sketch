5b25bb902081d99bb92c7bffdc1b84a74cec35c8
Lyn <wenzhan.main@gmail.com>
2018-08-04 22:39:32 -0700

    changes

--- resources/views/books/_book_selector.blade.php
+++ resources/views/books/_book_selector.blade.php
@@ -1,5 +1,5 @@
 
-<div class="dropdown">
+<div>
     <form method="POST" action="{{ route('books.filter') }}"  name="book_filter">
         {{ csrf_field() }}
         <div class="">
@@ -29,47 +29,39 @@
         </div>
 
         <div class="">
-            <h4>篇幅：</h4>
+            <h4>性向：</h4>
             <div class="">
-                @foreach($book_info['book_lenth_info'] as $key=>$book_lenth)
-                <input type="checkbox" name="length[]" value={{$key}} checked />&nbsp;{{$book_lenth}}&nbsp;&nbsp;&nbsp;
+                @foreach($book_info['sexual_orientation_info'] as $key=>$sexual_orientation)
+                <input type="checkbox" name="sexual_orientation[]" value={{$key}} checked />&nbsp;{{$sexual_orientation}}&nbsp;&nbsp;&nbsp;
                 @endforeach
             </div>
         </div>
 
         <div class="">
-            <h4>篇幅：</h4>
+            <h4>限制：</h4>
             <div class="">
-                @foreach($book_info['book_lenth_info'] as $key=>$book_lenth)
-                <input type="checkbox" name="length[]" value={{$key}} checked />&nbsp;{{$book_lenth}}&nbsp;&nbsp;&nbsp;
+                @foreach($book_info['rating_info'] as $key=>$rating)
+                <input type="checkbox" name="rating[]" value={{$key}} checked />&nbsp;{{$rating}}&nbsp;&nbsp;&nbsp;
                 @endforeach
             </div>
         </div>
 
-        <span class="button-group">
-            <button type="button" class="btn btn-default btn-xs dropdown-toggle dropdown-menu-narrow" data-toggle="dropdown">进度<span class="caret"></span></button>
-            <ul class="dropdown-menu">
-                @foreach($book_info['book_status_info'] as $key=>$book_status)
-                <li><input type="checkbox" name="status[]" value={{$key}} checked />&nbsp;{{$book_status}}</li>
+        <div class="">
+            <h4>各色标签：</h4>
+            <div class="">
+                <?php $tag_info = 0; ?>
+                @foreach($all_book_tags['tags'] as $key=>$tag)
+                    @if((Auth::check())||($tag->tag_group!==5))
+                        @if(($tag_info<$tag->tag_info)&&($tag_info>0))
+                            <br>
+                        @endif
+                        <input type="checkbox" name="tag[]" value={{$tag->id}} />&nbsp;{{ $tag->tagname }}&nbsp;&nbsp;&nbsp;
+                        <?php $tag_info = $tag->tag_info ?>
+                    @endif
                 @endforeach
-            </ul>
-        </span>
-        <span class="button-group">
-            <button type="button" class="btn btn-default btn-xs dropdown-toggle dropdown-menu-narrow" data-toggle="dropdown">性向<span class="caret"></span></button>
-            <ul class="dropdown-menu">
-                @foreach($book_info['sexual_orientation_info'] as $key=>$sexual_orientation)
-                <li><input type="checkbox" name="sexual_orientation[]" value={{$key}} checked />&nbsp;{{$sexual_orientation}}</li>
-                @endforeach
-            </ul>
-        </span>
-        <span class="button-group">
-            <button type="button" class="btn btn-default btn-xs dropdown-toggle dropdown-menu-narrow" data-toggle="dropdown">限制<span class="caret"></span></button>
-            <ul class="dropdown-menu">
-                @foreach($book_info['rating_info'] as $key=>$rating)
-                <li><input type="checkbox" name="rating[]" value={{$key}} checked />&nbsp;{{$rating}}</li>
-                @endforeach
-            </ul>
-        </span>
+            </div>
+        </div>
+
         <button type="submit" name="button" class="btn btn-xs btn-primary sosad-button">提交</button>
     </form>
 </div>
