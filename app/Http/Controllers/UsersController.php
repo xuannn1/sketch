d72a0bfd6312f3e587b3402014a3e654c3eef1ed
Lyn <wenzhan.main@gmail.com>
2018-08-10 04:43:58 -0700

    addrecords

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -142,6 +142,54 @@ class UsersController extends Controller
     //     }
     // }
 
+    public function findAdminRecords($id, $paginate)
+    {
+        $records = DB::table('administrations')
+        ->join('users','administrations.user_id','=','users.id')
+        ->leftjoin('threads',function($join)
+        {
+            $join->whereIn('administrations.operation',[1,2,3,4,5,6,9,15,16]);
+            $join->on('administrations.item_id','=','threads.id');
+        })
+        ->leftjoin('posts',function($join)
+        {
+            $join->whereIn('administrations.operation',[7,10,11,12]);
+            $join->on('administrations.item_id','=','posts.id');
+        })
+        ->leftjoin('post_comments',function($join)
+        {
+            $join->where('administrations.operation','=',8);
+            $join->on('administrations.item_id','=','post_comments.id');
+        })
+        ->leftjoin('users as operated_users',function($join)
+        {
+            $join->whereIn('administrations.operation',[13,14]);
+            $join->on('administrations.item_id','=','operated_users.id');
+        })
+
+        ->where(function ($query) use ($id) {
+            $query->whereIn('administrations.operation',[1,2,3,4,5,6,9,15,16]);
+            $query->where('threads.user_id', '=', $id);
+        })
+        ->orWhere(function ($query) use ($id) {
+            $query->whereIn('administrations.operation',[7,10,11,12]);
+            $query->where('posts.user_id', '=', $id);
+        })
+        ->orWhere(function ($query) use ($id) {
+            $query->where('administrations.operation','=',8);
+            $query->where('post_comments.user_id', '=', $id);
+        })
+        ->orWhere(function ($query) use ($id) {
+            $query->whereIn('administrations.operation',[13,14]);
+            $query->where('administrations.item_id', '=', $id);
+        })
+        ->where('administrations.deleted_at','=',null)
+        ->select('users.name','administrations.*','threads.title as thread_title','posts.body as post_body','post_comments.body as postcomment_body','operated_users.name as operated_users_name' )
+        ->orderBy('administrations.created_at','desc')
+        ->simplePaginate($paginate);
+        return $records;
+    }
+
     public function show($id)
     {
         $user = User::find($id);
