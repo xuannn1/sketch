de332d8a03790b9c1fcc801cbaf0f956c8eec001
Lyn <wenzhan.main@gmail.com>
2018-08-10 22:12:38 -0700

    redis

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -134,13 +134,13 @@ class PagesController extends Controller
 
     public function help()
     {
-        $users_online = Cache::remember('-users-online-count', 20, function () {
-            $users_online = DB::table('logging_statuses')
-            ->where('logged_on','>', time()-60*30)
-            ->count();
-            return $users_online;
-        });
-
+        // $users_online = Cache::remember('-users-online-count', 20, function () {
+        //     $users_online = DB::table('logging_statuses')
+        //     ->where('logged_on','>', time()-60*30)
+        //     ->count();
+        //     return $users_online;
+        // });
+        $users_online = 0;
         $data = config('constants');
         $webstat = WebStat::where('id','>',1)->orderBy('created_at', 'desc')->first();
         return view('pages/help',compact('data','webstat','users_online'));
