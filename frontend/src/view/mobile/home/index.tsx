47376b71e5c55858964d5eabe5b462a41ece85ec
necolo <negko311@gmail.com>
2018-10-22 14:23:51 +0800

    fix navbar top

--- frontend/src/view/mobile/home/index.tsx
+++ frontend/src/view/mobile/home/index.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { Core } from '../../../core';
-import { HomeNav_m, HomeNavE } from './nav';
 import { HomeDefault_m } from './default';
 import { Article_m } from './article';
 import { Forum_m } from './forum';
+import { NavTop } from '../../components/common';
 
 interface Props {
     core:Core;
@@ -13,14 +13,28 @@ interface State {
     nav:HomeNavE;
 }
 
+export enum HomeNavE {
+    default,
+    article,
+    forum,
+}
+
 export class Home_m extends React.Component<Props, State> {
     public state = {
         nav: HomeNavE.default,
     };
 
+    public handleNav = (nav:HomeNavE) => {
+        this.setState({nav});
+    }
+
     public render () {
         return (<div>
-            <HomeNav_m core={this.props.core} nav={(page:HomeNavE) => this.setState({ nav:page })} />
+            <NavTop items={[
+                {to:HomeNavE.default, label: '首页', onClick:this.handleNav},
+                {to:HomeNavE.article, label: '文库', onClick:this.handleNav},
+                {to:HomeNavE.forum, label: '论坛', onClick:this.handleNav}, 
+            ]} />
             <div>
                 { this.renderContent() }
             </div>
