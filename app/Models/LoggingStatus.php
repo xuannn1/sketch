d6e8cac89245c12b478bbc6efc0fafb05f6603ec
Lyn <wenzhan.main@gmail.com>
2018-12-12 15:00:11 -0800

    mess

--- app/Models/LoggingStatus.php
+++ app/Models/LoggingStatus.php
@@ -7,8 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class LoggingStatus extends Model
 {
     protected $guarded = [];
-    protected $primaryKey = 'ip'; // or null
 
     public $timestamps = false;
-    public $incrementing = false;
 }
--- database/migrations/2018_07_27_132302_create_logging_statuses_table.php
+++ database/migrations/2018_07_27_132302_create_logging_statuses_table.php
@@ -14,6 +14,7 @@ class CreateLoggingStatusesTable extends Migration
     public function up()
     {
         Schema::create('logging_statuses', function (Blueprint $table) {
+            $table->increments('id');
             $table->integer('user_id')->unsigned()->index()->default(0);//登陆的用户名
             $table->integer('logged_on')->unsigned()->default(0);//在机器时间的什么时候登陆
             $table->string('ip', 45)->nullable();
