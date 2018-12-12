c8bab495fd45055ea233e23ea6e4aceb3eaa9741
Lyn <wenzhan.main@gmail.com>
2018-12-12 14:51:56 -0800

    eloquent id

--- app/Models/LoggingStatus.php
+++ app/Models/LoggingStatus.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class LoggingStatus extends Model
 {
     protected $guarded = [];
+    protected $primaryKey = 'ip'; // or null
+
     public $timestamps = false;
-    //
+    public $incrementing = false;
 }
