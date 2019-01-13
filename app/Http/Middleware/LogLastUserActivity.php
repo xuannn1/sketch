c28ebaa656054928d3b1f7cf1728dd85b1f790d9
Lyn <wenzhan.main@gmail.com>
2019-01-13 00:44:00 -0800

    reduce counting clicks

--- app/Http/Middleware/LogLastUserActivity.php
+++ app/Http/Middleware/LogLastUserActivity.php
@@ -18,11 +18,12 @@ class LogLastUserActivity
     {
         if(Auth::check()) {
             //统计注册用户在线数量（仅统计在默认登陆时间间隔内在线的人，避免过度操作数据库）
-            if (Cache::has('-usr-clicks-'.Auth::id())){
-                Cache::increment('-usr-clicks-'.Auth::id());
-            }else{
-                Cache::put('-usr-clicks-'.Auth::id(),1,Carbon::now()->addDay(1));
-            }
+            //减少负荷，暂时取消
+            // if (Cache::has('-usr-clicks-'.Auth::id())){
+            //     Cache::increment('-usr-clicks-'.Auth::id());
+            // }else{
+            //     Cache::put('-usr-clicks-'.Auth::id(),1,Carbon::now()->addDay(1));
+            // }
             if(!Cache::has('-usr-on-' . Auth::id())){//假如距离上次cache的时间已经超过了默认时间
                 $record = LoggingStatus::updateOrCreate([
                     'user_id' => Auth::id(),
@@ -32,8 +33,8 @@ class LogLastUserActivity
                 ]);
                 $expiresAt = Carbon::now()->addMinutes(config('constants.online_count_interval'));
                 Cache::put('-usr-on-' . Auth::id(), true, $expiresAt);
-                Auth::user()->increment('daily_clicks', (int)Cache::pull('-usr-clicks-'.Auth::id()));
-                Auth::user()->reward('online_reward');
+                //Auth::user()->increment('daily_clicks', (int)Cache::pull('-usr-clicks-'.Auth::id()));
+                //Auth::user()->reward('online_reward');
             }
         }
         return $next($request);
