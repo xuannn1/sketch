006f903387de20f1de693e408be4fbdb56e3d1ff
Lyn <wenzhan.main@gmail.com>
2019-01-20 00:58:08 -0800

    validation error

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,16 +4,17 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use PDOException;
 
 class Handler extends ExceptionHandler
 {
     /**
-     * A list of the exception types that should not be reported.
-     *
-     * @var array
-     */
+    * A list of the exception types that should not be reported.
+    *
+    * @var array
+    */
     protected $dontReport = [
         \Illuminate\Auth\AuthenticationException::class,
         \Illuminate\Auth\Access\AuthorizationException::class,
@@ -24,31 +25,35 @@ class Handler extends ExceptionHandler
     ];
 
     /**
-     * Report or log an exception.
-     *
-     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-     *
-     * @param  \Exception  $exception
-     * @return void
-     */
+    * Report or log an exception.
+    *
+    * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+    *
+    * @param  \Exception  $exception
+    * @return void
+    */
     public function report(Exception $exception)
     {
         parent::report($exception);
     }
 
     /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
-     */
+    * Render an exception into an HTTP response.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  \Exception  $exception
+    * @return \Illuminate\Http\Response
+    */
     public function render($request, Exception $exception)
     {
         if($exception instanceof PDOException ){
             return abort(595);
         }
 
+
+        if ($exception instanceof ValidationException) {
+            return parent::render($request, $exception);
+        }
         if ($exception instanceof Exception) {
             return response()->error($exception, 599);
         }
@@ -56,12 +61,12 @@ class Handler extends ExceptionHandler
     }
 
     /**
-     * Convert an authentication exception into an unauthenticated response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Illuminate\Auth\AuthenticationException  $exception
-     * @return \Illuminate\Http\Response
-     */
+    * Convert an authentication exception into an unauthenticated response.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  \Illuminate\Auth\AuthenticationException  $exception
+    * @return \Illuminate\Http\Response
+    */
     protected function unauthenticated($request, AuthenticationException $exception)
     {
         if ($request->expectsJson()) {
