03eda75ec273b3e8f4bb456bc92379c3b2cbbe6c
Lyn <wenzhan.main@gmail.com>
2019-01-20 02:48:28 -0800

    resolve changes in test

--- backend/tests/Feature/StoreQuoteTest.php
+++ backend/tests/Feature/StoreQuoteTest.php
@@ -13,24 +13,14 @@ class StoreQuoteTest extends TestCase
     {
         $user = $this->actingAs(factory('App\Models\User')->create(), 'api');
 
-<<<<<<< HEAD
-        $body = "testtestttt";
-        $request = $this->actingAs($user, 'api')
-        ->post('api/quote', ['body' => $body]);
-=======
         $body = "testtesttest";
         $request = $this->post('api/quote', ['body' => $body]);
->>>>>>> dfbfc7c63b8894514661c0a1eeb10c462079e7ca
 
         $response = $request->send();
 
         $this->assertEquals(200, $response->getStatusCode());
 
-<<<<<<< HEAD
-        $body = "testtestttt";
-=======
         $body = "testtesttest";
->>>>>>> dfbfc7c63b8894514661c0a1eeb10c462079e7ca
         $request = $this->post('api/quote', ['body' => $body]);
 
         $response = $request->send();
@@ -41,11 +31,7 @@ class StoreQuoteTest extends TestCase
     /** @test */
     public function a_guest_can_not_create_quote()
     {
-<<<<<<< HEAD
-        $body = "test7tt";
-=======
         $body = "test8";
->>>>>>> dfbfc7c63b8894514661c0a1eeb10c462079e7ca
         $request = $this->post('api/quote', ['body' => $body]);
 
         $response = $request->send();
