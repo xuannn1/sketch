99c874ff215c529dcd81cbac5d137ee59ec23b99
Erica-WX <2544194336@qq.com>
2019-03-20 21:38:40 +0800

    书本预览

--- frontend/src/view/components/book/book-preview.tsx
+++ frontend/src/view/components/book/book-preview.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { ResData } from '../../../config/api';
 import { Link } from 'react-router-dom';
+import { parseDate } from '../../../utils/date';
 
 // todo: mini version
 interface Props {
@@ -13,13 +14,17 @@ interface State {
 export class BookPreview extends React.Component<Props, State> {
     public render () {
         const { attributes, id, author, tags, last_component } = this.props.data;
+        const mini = this.props.mini || false;
+
         return <div className="book-item" key={id}>
             <Link className="title" to={`/book/${id}`}>{ attributes.title }</Link>
+            {!mini && !attributes.is_bianyuan && <span className="tag" style={{color: 'red'}}>边</span>}
+
             <div className="biref">{ attributes.brief }</div>
-            { last_component &&
+            { !mini && last_component &&
                 <span className="latest-chapter">最新章节: { last_component.attributes.title }</span>
             }
-            { tags &&
+            { !mini && tags &&
                 <div className="tags">
                     { tags.map((tag, i) => 
                         <Link className="tag" key={i} to={`/books/?tags=[${tag.id}]`}>{tag.attributes.tag_name}</Link>
@@ -28,11 +33,20 @@ export class BookPreview extends React.Component<Props, State> {
             }
             <div className="meta">
                 <Link className="author" to={`/user/${author.id}`}>{author.attributes.name}</Link>
-                <div className="counters">
+
+                { mini && attributes.created_at && attributes.edited_at &&
+                <span className="date">
+                    {parseDate(attributes.created_at)}/{parseDate(attributes.edited_at)}
+                </span>
+                }
+
+                {
+                  !mini &&  <div className="counters">
                     <span><i className="fas fa-pencil-alt"></i>{attributes.total_char}</span> /
                     <span><i className="fas fa-eye"></i>{attributes.view_count}</span> / 
                     <span><i className="fas fa-comment-alt"></i>{attributes.reply_count}</span> /
                 </div>
+                }
             </div>
         </div>;
     }
