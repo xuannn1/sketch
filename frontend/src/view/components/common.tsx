532717e50aa4f457709eba79b4a3a4e938935ea4
cyan <504512494@qq.com>
2019-04-04 13:41:40 +0800

    add recom-preview

--- frontend/src/view/components/common.tsx
+++ frontend/src/view/components/common.tsx
@@ -233,3 +233,23 @@ export function List (props:{
         })}
     </div>;
 }
+
+export function Slider (props: {
+    children: React.ReactNode[],
+    morePath: string,
+}) {
+    return <div className="slider-wrapper">
+        <div className="slider-list">
+            {props.children.map((child,idx) => {
+                return <div className="item" key={idx}>
+                    {child}
+                </div>
+            })}
+            <div className="item">
+                <Link className="more" to={props.morePath}>
+                    更多
+                </Link>
+            </div>
+        </div>
+    </div>
+}
