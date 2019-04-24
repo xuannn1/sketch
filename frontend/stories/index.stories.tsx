82b2239c78632d716891b3237de75fdfd655086f
Erica-WX <2544194336@qq.com>
2019-04-24 21:43:56 +0800

    modify

--- frontend/stories/index.stories.tsx
+++ frontend/stories/index.stories.tsx
@@ -1,45 +1,30 @@
 import * as React from 'react';
 import { storiesOf, addDecorator } from '@storybook/react';
-import { Carousel } from '../src/view/components/common/carousel';
-import { Badge } from '../src/view/components/common/badge';
 import { withViewport } from '@storybook/addon-viewport';
 import { withConsole } from '@storybook/addon-console';
-import { withKnobs, boolean } from '@storybook/addon-knobs';
+import { withKnobs } from '@storybook/addon-knobs';
 import { Core } from '../src/core';
-// import { Chapter } from '../src/view/components/book/chapter';
 import '../src/theme.scss';
-import { pageDecorator } from './decorator';
+//import { Accordion } from '../src/view/components/common/accordion';
+import { Badge } from '../src/view/components/common/badge';
 
 // import { action } from '@storybook/addon-actions';
 
-const core = new Core();
-
-
-
 addDecorator((storyFn, context) => withConsole()(storyFn)(context));
 
-storiesOf('Home', module)
-  .addDecorator(withViewport())
-  .addDecorator(withKnobs)
-  .addDecorator(pageDecorator)
-  .add('Carousel', () => 
-    <Carousel
-      windowResizeEvent={core.windowResizeEvent}
-      slides={[
-      <span>one</span>,
-      <span>two</span>,
-      <span>three</span>,
-    ]}
-    indicator={boolean(true)} />
-  )
-;
 
 let value:number = 10;
 let max:number = 99;
-storiesOf('Home', module)
+storiesOf('Common Components', module)
   .addDecorator(withViewport())
-  .addDecorator(withKnobs)
-  .addDecorator(pageDecorator)
+  .addDecorator(withKnobs) 
+  
+  /* .add('Accordion', () => <Accordion 
+    title={'test'}
+    items={[]} 
+  />) */
+
+  // 在后面加入其他组件
   .add('Badge1', () => 
     <Badge num={value}>
       <span>
@@ -47,12 +32,6 @@ storiesOf('Home', module)
       </span>
     </Badge>
   )
-;
-
-storiesOf('Home', module)
-  .addDecorator(withViewport())
-  .addDecorator(withKnobs)
-  .addDecorator(pageDecorator)
   .add('Badge2', () => 
     <Badge num={value+90} max={max}>
       <span>
@@ -60,13 +39,6 @@ storiesOf('Home', module)
       </span>
     </Badge>
   )
-;
-
-
-storiesOf('Home', module)
-  .addDecorator(withViewport())
-  .addDecorator(withKnobs)
-  .addDecorator(pageDecorator)
   .add('Badge3', () => 
     <Badge dot>
       <span>
@@ -74,12 +46,6 @@ storiesOf('Home', module)
       </span>
     </Badge>
   )
-;
-
-storiesOf('Home', module)
-  .addDecorator(withViewport())
-  .addDecorator(withKnobs)
-  .addDecorator(pageDecorator)
   .add('Badge4', () => 
     <Badge hidden>
       <span>
@@ -88,17 +54,18 @@ storiesOf('Home', module)
     </Badge>
   )
 ;
-// storiesOf('Book', module)
-//   .addDecorator(withViewport())
-//   .addDecorator(withKnobs)
-//   .add('Chapter', () =>
-//     <Chapter chapter={{
-//       type: 'chapter',
-//       id: 0,
-//       attributes: {
-//       title: '',
-//       body: '',
-//       }
-//     }} /> 
-//   )
-// ;
+
+storiesOf('Home Components', module)
+.addDecorator(withViewport())
+.addDecorator(withKnobs)
+;
+
+storiesOf('User Components', module)
+.addDecorator(withViewport())
+.addDecorator(withKnobs)
+;
+
+storiesOf('Thread Components', module)
+.addDecorator(withViewport())
+.addDecorator(withKnobs)
+;
