97852f232c4e19908c09d0f8d9e19e66ab5eb706
Lyn <wenzhan.main@gmail.com>
2019-07-23 02:27:27 -0700

    vote post update

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -193,7 +193,7 @@
             <div class="text-right post-vote">
                 <span><a href="#" data-id="{{$post->id}}" data-toggle="modal" data-target="#TriggerPostReward{{ $post->id }}" class="btn btn-default btn-md">打赏</a></span>
                 @if(Auth::user()->level >= 1)
-                    <span class="voteposts"><button class="btn btn-default btn-md" data-id="{{$post->id}}"  id = "{{$post->id.'upvote'}}" onclick="voteItem('post', {{$post->id}},'upvote')" ><span class="glyphicon glyphicon-heart">{{$post->upvote_count}}</span></button></span>
+                    <span class="voteposts"><button class="btn btn-default btn-md" data-id="{{$post->id}}" onclick="voteItem('post', {{$post->id}},'upvote')" ><span class="glyphicon glyphicon-heart"><span id="post{{$post->id}}upvote">{{ $post->upvote_count }}</span></span></button></span>
                 @endif
                 @if((!$thread->is_locked)&&(!$thread->noreply)&&(!Auth::user()->no_posting)&&($post->fold_state==0)&&(Auth::user()->level >= 2))
                     &nbsp;<span ><a href = "#replyToThread" class="btn btn-default btn-md" onclick="replytopost({{ $post->id }}, '{{ StringProcess::trimtext($post->title.$post->brief, 40) }}')"><span class="glyphicon glyphicon-comment">{{ $post->reply_count }}</span></a></span>
