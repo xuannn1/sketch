2ef2d0b613a247b58f7cd30f7410285c048a8492
Haochang Qian <fakedestinyck@gmail.com>
2020-01-27 21:56:44 -0500

    Add quiz test

--- backend/tests/Feature/RegistrationByInvitationEmailTest.php
+++ backend/tests/Feature/RegistrationByInvitationEmailTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Http\Resources\QuizOptionResource;
+use App\Models\QuizOption;
+use App\Models\RegistrationApplication;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -29,7 +32,7 @@ class RegistrationByInvitationEmailTest extends TestCase
             ->assertStatus(422);
 
         // 验证返回题目和格式
-        $data['email'] = $this->faker->email;
+        $data['email'] = 'tester7@tester.com';
         $response = $this->post('api/register/by_invitation_email/submit_email', $data);
         $response->assertStatus(200)
             ->assertJsonStructure([
@@ -38,7 +41,15 @@ class RegistrationByInvitationEmailTest extends TestCase
                     'registration_application' => [
                         'id',
                         'type',
-                        'attributes'
+                        'attributes' => [
+                            'email',
+                            'has_quizzed',
+                            'email_verified_at',
+                            'submitted_at',
+                            'is_passed',
+                            'last_invited_at',
+                            'is_in_cooldown'
+                        ]
                     ],
                     'quizzes' => [
                         '*' => [
@@ -60,5 +71,23 @@ class RegistrationByInvitationEmailTest extends TestCase
                 ],
             ]);
 
+        // 验证是否取到了正确的题目
+        $response=$response->decodeResponseJson();
+        $id = $response["data"]["registration_application"]["id"];
+        $quizzes_questions = RegistrationApplication::find($id)->quiz_questions;
+        $returned_quizzes_questions = [];
+        $this->assertCount(config('constants.registration_quiz_total'),$response["data"]["quizzes"]);
+        foreach ($response["data"]["quizzes"] as $quiz) {
+            $returned_quizzes_questions[] = $quiz["id"];
+            $this->assertDatabaseHas('quizzes', ["id" => $quiz["id"]]);
+            // 这里是验证是否所有选项都被选出来了
+            $options_from_database = QuizOption::where('quiz_id',$quiz['id'])->orderBy('id')->pluck('id')->toArray();
+            $options_from_returned = [];
+            foreach ($quiz["attributes"]["options"] as $option) {
+                $options_from_returned[] = $option['id'];
+            }
+            $this->assertEquals($options_from_database,$options_from_returned);
+        }
+        $this->assertEquals($quizzes_questions,implode(",",$returned_quizzes_questions));
     }
 }
