ebebb6f5ce7ad1a3aa8911f87eae398401c8d449
EmolLi <duan.li@mail.mcgill.ca>
2020-01-29 18:06:56 -0500

    [UI] connect pm with api

--- frontend/src/view/mobile/message/personal-msg.tsx
+++ frontend/src/view/mobile/message/personal-msg.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { API, ResData, ReqData } from '../../../config/api';
 import { MobileRouteProps } from '../router';
 // import { StatusNav } from './nav';
 import { Page } from '../../components/common/page';
@@ -9,12 +10,32 @@ import { Badge } from '../../components/common/badge';
 import { List } from '../../components/common/list';
 import { pageStyle, largeListItemStyle, badgeStyle, topCardStle, contentCardStyle, replyNotificationCardStyle, replyMessageContentStyle, unreadStyle, oneLineTruncationStyle } from './styles';
 import { mockReplyNotifications } from './mock-data';
+import { Message } from '.';
 
 interface State {
-
+  data:API.Get['/user/$0/message'];
 }
 
 export class PersonalMessage extends React.Component<MobileRouteProps, State> {
+  public state:State = {
+    data:{
+      messages: [],
+      paginate: ResData.allocThreadPaginate(),
+      style: ReqData.Message.style.receiveBox,
+    },
+  };
+
+  public async componentDidMount() {
+    try {
+      const query = {withStyle: ReqData.Message.style.receiveBox};
+      const data = await this.props.core.db.getMessages(query);
+      this.setState({data});
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   public render () {
     return (<Page style={pageStyle}
         top={<NavBar goBack={this.props.core.history.goBack} onMenuClick={() => console.log('open setting')}>
@@ -38,17 +59,23 @@ export class PersonalMessage extends React.Component<MobileRouteProps, State> {
           </List>
         </Card>
         {/* reply notifications */}
-        <Card style={replyNotificationCardStyle}>
-        <List style={{background:'transparent'}}>
-          {mockReplyNotifications.map((n, i) =>
-          <List.Item key={i} style={{background:'white', marginBottom:'0.3em'}}>
-            <h6 className="is-6" style={n.read ? {} : unreadStyle}>{n.author}</h6>
-            <div style={replyMessageContentStyle}>
-              <p style={oneLineTruncationStyle}>{!n.read ? <React.Fragment><b>[有新消息]</b>{` `}</React.Fragment> : ''}{n.message}</p>
-            </div>
-          </List.Item>)}
-        </List>
-        </Card>
+        { this.renderMessages() }
       </Page>);
+  }
+
+  private renderMessages () {
+    const { messages } = this.state.data;
+    // TODO: mark msg as read
+    return (<Card style={replyNotificationCardStyle}>
+              <List style={{background:'transparent'}}>
+                {messages.map((m, i) =>
+                  <List.Item key={i} style={{background:'white', marginBottom:'0.3em'}}>
+                    <h6 className="is-6" style={m.attributes.seen ? {} : unreadStyle}>{m.poster ? m.poster.attributes.name : ''}</h6>
+                    <div style={replyMessageContentStyle}>
+                      <p style={oneLineTruncationStyle}>{!m.attributes.seen ? <React.Fragment><b>[有新消息]</b>{` `}</React.Fragment> : ''}{m.message_body ? m.message_body.attributes.body : ''}</p>
+                    </div>
+                  </List.Item>)}
+              </List>
+            </Card>);
   }
 }
