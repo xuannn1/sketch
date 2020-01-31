b25fc2a8d5f33831ea9a6334ea55d6be0c407625
EmolLi <duan.li@mail.mcgill.ca>
2020-01-31 12:35:47 -0500

    [frontend] remove store

--- frontend/src/core/store.ts
+++ frontend/src/core/store.ts
@@ -1,15 +1,16 @@
 // a global store is used to save some temporary data shared between components. (e.g. poster name for dialogue)
 // when some data is hard to pass around between components, (e.g. one of the component is in route root level), we can put the data in the temporary store
 
-export class Store {
-  public dialogue:{
-    recipientName:string,
-  };
+// FIXME: it seems that we do not really, probably delete it in future
+// export class Store {
+//   public dialogue:{
+//     recipientName:string,
+//   };
 
-  public constructor() {
-    this.dialogue = {
-      recipientName:'EMOL',
-    };
-  }
+//   public constructor() {
+//     this.dialogue = {
+//       recipientName:'',
+//     };
+//   }
 
-}
+// }
