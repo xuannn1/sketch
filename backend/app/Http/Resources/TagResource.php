abd5117b577a5be00df0306f40e5ce1b8689aec6
Lyn <wenzhan.main@gmail.com>
2020-02-08 23:20:49 -0800

    tagresource

--- backend/app/Http/Resources/TagResource.php
+++ backend/app/Http/Resources/TagResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class TagBriefResource extends JsonResource
+class TagResource extends JsonResource
 {
     /**
     * Transform the resource into an array.
